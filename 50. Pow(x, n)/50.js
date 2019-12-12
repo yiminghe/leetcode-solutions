ffe7f9a15101bec56e5303913b379282986e6729
yiminghe <yiminghe@gmail.com>
2019-12-12 15:37:35 +0800

    50

---
+++ 50. Pow(x, n)/50.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+  if(n===0){
+    return 1;
+  }
+  let sign=1;
+  if(n<0){
+    n=-n;
+    sign=-1;
+  }
+  let ans=1;
+  while(n){
+    if(n%2){
+      ans*=x;
+      n--;
+    }
+    x=x*x;
+    n=n/2|0;
+  }
+  return ans;
+};
+
+let x=4;
+let n=113;
+
+console.log(myPow(x,n) ==Math.pow(x,n));
