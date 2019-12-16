b3eb83665cf422b17ca82df55264b95cde350d1b
yiminghe <yiminghe@gmail.com>
2019-12-16 21:53:30 +0800

    add visualizeTree

--- 112. Path Sum/112.js
+++ 112. Path Sum/112.js
@@ -10,37 +10,38 @@
  * @param {number} sum
  * @return {boolean}
  */
-var hasPathSum = function(root, sum, path,tag='root') {
-  if(root){
+var hasPathSum = function (root, sum, path, tag = 'root') {
+  if (root) {
     sum -= root.val;
-    path.push(tag+': '+root.val);
-    if(!root.left && !root.right){
+    path.push(tag + ': ' + root.val);
+    if (!root.left && !root.right) {
       const ret = (sum === 0);
-      if(!ret){
+      if (!ret) {
         path.pop();
       } else {
-      console.log(path);
+        console.log(path);
       }
       return ret;
     }
-    const left = hasPathSum(root.left, sum,path,'l');
-    const right = hasPathSum(root.right, sum,path,'r');
+    const left = hasPathSum(root.left, sum, path, 'l');
+    const right = hasPathSum(root.right, sum, path, 'r');
     path.pop();
     return left || right;
   }
   return false;
 };
 
-(function(){
-  const {createTree,visualizeTree} = require('../utils/tree');
+(function () {
+  const { createTree, visualizeTree } = require('../utils/tree');
 
-  const treeArr = [5,4,8,11,null,13,4,7,2,null,null,null,1];
-  const root = createTree(treeArr.concat());
+  const treeArr = [5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1];
+  let root = createTree(treeArr.concat());
   // console.log(JSON.stringify(root,null,2));
 
-  console.log(hasPathSum(root,22,[]));
-  
-  console.log(visualizeTree(treeArr));
+  console.log(hasPathSum(root, 22, []));
+
+  root = (visualizeTree(treeArr));
+  // console.log(JSON.stringify(root,null,2));
 })();
 
 
--- utils/tree.js
+++ utils/tree.js
@@ -1,85 +1,124 @@
 
 
-function write(s){
-  process.stdout.write(s+'');
+function write(s) {
+  process.stdout.write(s + '');
 }
 
-function getN(n,l){
-  var s=n;
-  const prefix=typeof s !=='number'?s:'0';
-  s+='';
-  while(s.length<l){
-    s=prefix+s;
+function padding(n, l) {
+  var s = n;
+  const prefix = typeof s !== 'number' ? s : '0';
+  s += '';
+  while (s.length < l) {
+    s = prefix + s;
   }
   return s;
 }
 
-function getL(arr){
-  let l=-1;
-  arr.forEach(t=>{
-    if(t){
-      t+='';
-      if(t.length>l){
-        l=t.length;
+function getMaxWidth(arr) {
+  let l = -1;
+  arr.forEach(t => {
+    if (t) {
+      t += '';
+      if (t.length > l) {
+        l = t.length;
       }
     }
   });
   return l;
 }
 
-function getLevel(arr){
+function getLevel(arr) {
   const l = arr.length;
-  let index=0;
-  let limit=1;
+  let index = 0;
+  let limit = 1;
   let element;
-  let level=0;
-  while(index<l){
-    let count=0;
-    while(index<limit && index<l){
-      element=arr[index];
-      if(element){
+  let level = 0;
+  while (index < l) {
+    let count = 0;
+    while (index < limit && index < l) {
+      element = arr[index];
+      if (element) {
         count++;
       }
       index++;
     }
     level++;
-    limit+=count*2;
+    limit += count * 2;
   }
   return level;
 }
 
 const api = module.exports = {
-  createTree(arr){
-    arr.forEach((element,index) => {
-      if(arr[index]){
-        arr[index]={
-          val:element,
+  createTree(arr) {
+    arr.forEach((element, index) => {
+      if (arr[index]) {
+        arr[index] = {
+          val: element,
         }
       }
     });
     const l = arr.length;
-    let index=0;
-    let limit=1;
+    let index = 0;
+    let limit = 1;
     let element;
-    while(index<l){
-      let count=0;
-      while(index<limit && index<l){
-        element=arr[index];
-        if(element){
+    let level = 0;
+    while (index < l) {
+      let count = 0;
+      while (index < limit && index < l) {
+        element = arr[index];
+        if (element) {
           const base = count;
-          const left = limit+2*base;
-          const right=left+1;
-          element.left=arr[left];
-          element.right=arr[right];
+          const left = limit + 2 * base;
+          const right = left + 1;
+          element.left = arr[left];
+          element.right = arr[right];
+          element.level = level;
+          element.count = count;
           count++;
         }
         index++;
       }
-      limit+=count*2;
+      level++;
+      limit += count * 2;
     }
     return arr[0];
   },
-  visualizeTree(arr){
+  visualizeTree(arr) {
 
+    const maxWidth = getMaxWidth(arr);
+    const root = api.createTree(arr);
+    infix(root);
+    let currentLevel = 0;
+    let prevEl;
+    for (const currentElement of arr) {
+      if (currentElement) {
+        if (currentElement.level !== currentLevel) {
+          console.log('');
+          prevEl = null;
+          currentLevel = currentElement.level;
+        }
+        if (!prevEl) {
+          write(padding(' ', maxWidth).repeat(currentElement.order - 1));
+
+        } else {
+          write(padding(' ', maxWidth).repeat(currentElement.order - prevEl.order - 1));
+        }
+
+        prevEl = currentElement;
+
+        write(padding(currentElement.val, maxWidth));
+      }
+    }
+    console.log();
   }
 };
+
+function infix(root, order = [0]) {
+  if (!root) {
+    return;
+  }
+  infix(root.left, order);
+  root.order = ++order[0];
+  infix(root.right, order);
+
+}
