1ff87b17ed61d5b4cfd5c1fc06d82714adbc9ba4
yiminghe <yiminghe@gmail.com>
2019-12-17 16:27:45 +0800

    124

--- 124. Binary Tree Maximum Path Sum/124.js
+++ 124. Binary Tree Maximum Path Sum/124.js
@@ -9,26 +9,74 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxPathSum = function(root) {
-    
-};
+var maxPathSum = function (root) {
+  let ans = Number.MIN_SAFE_INTEGER;
+  let maxAnsPath;
 
+  function getMaxPath(n) {
+    const nval = n.val;
+    if (!n.left && !n.right) {
+      if (ans < nval) {
+        ans = nval;
+        maxAnsPath = `[${nval}]`;
+      }
+      n.maxPath = `[${nval}]`;
+      return nval;
+    }
 
-// [-10,9,20,null,null,15,7]
-function createTree(arr){
-  arr.forEach((element,index) => {
-    if(arr[index]){
-      arr[index]={
-        val:element,
+    let subAnsPath = `[${nval}`;
+    let subMaxPath = `[${nval}]`;
+    let max = nval;
+    let subAns = nval;
+    if (n.left) {
+      const maxleft = getMaxPath(n.left);
+      const nleftval = maxleft + nval;
+      if (nleftval > max) {
+        max = nleftval;
+        subMaxPath = `[${nval} l:${n.left.maxPath}]`;
+      }
+      if (subAns < nleftval) {
+        subAns = nleftval;
+        subAnsPath += ` l:${n.left.maxPath}`;
       }
     }
-  });
-  const l = arr.length;
-  arr.forEach((element,index) => {
-    const left = 2*index+1;
-    const right=left+1;
-    element.left=arr[left];
-    element.right=arr[right];
-  });
-  return arr[0];
-}
+    if (n.right) {
+      const maxright = getMaxPath(n.right);
+      const nrightval = maxright + nval;
+      if (nrightval > max) {
+        max = nrightval;
+        subMaxPath = `[${nval} r:${n.right.maxPath}]`;
+      }
+      if (maxright > 0) {
+        subAns += maxright;
+        subAnsPath += ` r:${n.right.maxPath}`;
+      }
+    }
+
+    if (ans < subAns) {
+      ans = subAns;
+      maxAnsPath = subAnsPath + ']';
+    }
+
+    n.maxPath = subMaxPath;
+
+    return max;
+  }
+  
+  getMaxPath(root);
+
+
+  console.log(maxAnsPath);
+
+  return ans;
+};
+
+const { createTree } = require('../utils/tree');
+
+let root = createTree([1, null, 2, null, 3, null, 4, null, 5]);
+root = createTree([-10, 9, 20, null, null, 15, 7]);
+
+const getNow = require('performance-now');
+const now = getNow();
+console.log(maxPathSum(root));
+console.log(getNow() - now);
