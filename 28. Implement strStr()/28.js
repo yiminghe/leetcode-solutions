97551f21e823cb42fc328504344a335b2583d74a
yiminghe <yiminghe@gmail.com>
2020-01-10 11:35:24 +0800

    28 kmp

---
+++ 28. Implement strStr()/28.js
@@ -0,0 +1,72 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (str, pattern) {
+  debugger
+  if (!pattern) {
+    return str?-1:0;
+  }
+  const patternLength = pattern.length;
+  const strLength = str.length;
+  let sindex = 0;
+  let pindex = 0;
+  let sc;
+  let pc;
+  const next = getNext(pattern);
+  console.log(next);
+  while (sindex < strLength && pindex !== patternLength) {
+    if (pindex !== -1) {
+      sc = str[sindex];
+      pc = pattern[pindex];
+    }
+    if (pindex === -1 || str[sindex] === pattern[pindex]) {
+      sindex++;
+      pindex++;
+    } else {
+      pindex = next[pindex];
+    }
+  }
+  if (pindex === patternLength) {
+    return sindex - pindex;
+  }
+  return -1;
+};
+
+function getNext(p) {
+  const l = p.length;
+  const next = new Array(l);
+  let i = -1;
+  let j = 0;
+  next[0] = -1;
+  while (j < l - 1) {
+    if (i === -1 || p[i] === p[j]) {
+      i++;
+      j++;
+      if(p[i]===p[j]){
+        let ii=i;
+        while(ii!==-1 && p[ii]===p[j]){
+          ii=next[j]=next[ii];
+        }
+      } else {
+        next[j] = i;
+      }
+    } else {
+      i = next[i];
+    }
+  }
+  return next;
+}
+
+let haystack = "hello", needle = "ll";
+console.log(strStr(haystack, needle));
+
+haystack = "aaaaa", needle = "bba";
+console.log(strStr(haystack, needle));
+
+needle = "abcdabd";
+console.log(getNext(needle));
+
+needle = "abab";
+console.log(getNext(needle));
