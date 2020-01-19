e27c8fd06d899a0aa3c69b79c52f4a60759eb34a
yiminghe <yiminghe@gmail.com>
2020-01-19 11:07:19 +0800

    3

---
+++ 3. Longest Substring Without Repeating Characters/3.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  const l = s.length;
+  let start = -1;
+  const map = new Array(255);
+  let ret = -1;
+  map.fill(start);
+  let occurenceIndex;
+  let charCode;
+  for (let end = 0; end < l; end++) {
+    charCode = s.charCodeAt(end);
+    occurenceIndex = map[charCode];
+    start = Math.max(start, occurenceIndex);
+    map[charCode] = end;
+    ret = Math.max(ret, end - start);
+  }
+  return ret;
+};
+
+
+console.log(lengthOfLongestSubstring('abccdf'));
