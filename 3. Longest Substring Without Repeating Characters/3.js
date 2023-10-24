2893ca0ad46c178e216317d2eb3a57f2c996500e
yiminghe <yiminghe@gmail.com>
2023-10-24 11:37:50 +0800

    typo

--- 3. Longest Substring Without Repeating Characters/3.js
+++ 3. Longest Substring Without Repeating Characters/3.js
@@ -8,12 +8,12 @@ var lengthOfLongestSubstring = function (s) {
   const map = new Array(255);
   let ret = -1;
   map.fill(start);
-  let occurenceIndex;
+  let occurrenceIndex;
   let charCode;
   for (let end = 0; end < l; end++) {
     charCode = s.charCodeAt(end);
-    occurenceIndex = map[charCode];
-    start = Math.max(start, occurenceIndex);
+    occurrenceIndex = map[charCode];
+    start = Math.max(start, occurrenceIndex);
     map[charCode] = end;
     ret = Math.max(ret, end - start);
   }
