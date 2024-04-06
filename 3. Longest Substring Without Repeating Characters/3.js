30beabf4bb399b19b37a44fefb00b7dd29d0583e
yiminghe <yiminghe@gmail.com>
2024-04-06 16:43:01 +0800

    optimize 3

--- 3. Longest Substring Without Repeating Characters/3.js
+++ 3. Longest Substring Without Repeating Characters/3.js
@@ -2,22 +2,58 @@
  * @param {string} s
  * @return {number}
  */
+
 var lengthOfLongestSubstring = function (s) {
   const l = s.length;
-  let start = -1;
+  if (l === 0) {
+    return 0;
+  }
+  let start = 0;
   const map = new Array(255);
-  let ret = -1;
-  map.fill(start);
+  let ret = 1;
   let occurrenceIndex;
   let charCode;
-  for (let end = 0; end < l; end++) {
+  for (let end = 0; l > ret + start; end++) {
     charCode = s.charCodeAt(end);
     occurrenceIndex = map[charCode];
-    start = Math.max(start, occurrenceIndex);
+    if (occurrenceIndex !== undefined) {
+      start = Math.max(start, occurrenceIndex + 1);
+    }
     map[charCode] = end;
-    ret = Math.max(ret, end - start);
+    ret = Math.max(ret, end - start + 1);
   }
   return ret;
 };
 
-console.log(lengthOfLongestSubstring('abccdf'));
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var lengthOfLongestSubstring2 = function (s) {
+  const l = s.length;
+  if (l === 0) {
+    return 0;
+  }
+  let start = 0;
+  const map = new Map();
+  let ret = 1;
+  let occurrenceIndex;
+  let charCode;
+  for (let end = 0; l > ret + start; end++) {
+    charCode = s[end];
+    occurrenceIndex = map.get(charCode);
+    if (occurrenceIndex !== undefined) {
+      start = Math.max(start, occurrenceIndex + 1);
+    }
+    map.set(charCode,end);
+    ret = Math.max(ret, end - start + 1);
+  }
+  return ret;
+};
+
+console.log(lengthOfLongestSubstring2('pwwkew'));
+
+
+console.log(lengthOfLongestSubstring('pwwkew'));
