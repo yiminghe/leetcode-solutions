b2661bc02659aee350db724e38c129466d4b7b7a
yiminghe <yiminghe@gmail.com>
2024-04-06 21:00:20 +0800

    518. Coin Change II

---
+++ 518. Coin Change II/518.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+  const dp = new Array(amount + 1);
+  dp.fill(0);
+  dp[0] = 1;
+  for (let coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] += dp[i - coin];
+    }
+  }
+  return dp[amount];
+};
+
+console.log(change(5, [1, 2, 5]));
