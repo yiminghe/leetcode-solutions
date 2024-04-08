850de72a5ba182aaf0b60f5ca72699f90d359cb3
yiminghe <yiminghe@gmail.com>
2024-04-08 16:32:23 +0800

    update 39

--- 39. Combination Sum/39.js
+++ 39. Combination Sum/39.js
@@ -33,7 +33,35 @@ var combinationSum = function (candidates, target) {
   return ret;
 };
 
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function (candidates, target) {
+  const ret = [];
+  function dfs(start, cur, path) {
+    if (cur === 0) {
+      ret.push(path.concat());
+      return;
+    }
+    if (cur < 0) {
+      return;
+    }
+    for (let i = start; i < candidates.length; i++) {
+      const c = candidates[i];
+      path.push(c);
+      dfs(i, cur - c, path);
+      path.pop();
+    }
+  }
+  dfs(0, target, []);
+  return ret;
+};
+
 const candidates = [2, 3, 5];
 const target = 8;
 
+console.log(combinationSum2(candidates, target));
 console.log(combinationSum(candidates, target));
